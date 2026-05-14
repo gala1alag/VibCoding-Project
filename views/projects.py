8c5e69eeadd633bde309d30415da746805e1c9ab
Wang Yilin <1925900631@qq.com>
2026-05-14 14:09:23 +0800

    feat: 组装 ProjectsView QSplitter 容器

--- views/projects.py
+++ views/projects.py
@@ -1,11 +1,25 @@
-from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QWidget, QHBoxLayout, QSplitter
 from PyQt5.QtCore import Qt
+from views.project_list import ProjectListWidget
+from views.project_detail import ProjectDetailWidget
+
 
 class ProjectsView(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
-        layout = QVBoxLayout(self)
-        label = QLabel("项目管理")
-        label.setAlignment(Qt.AlignCenter)
-        label.setStyleSheet("font-size: 24px; color: #4A90D9;")
-        layout.addWidget(label)
+        layout = QHBoxLayout(self)
+        layout.setContentsMargins(0, 0, 0, 0)
+
+        splitter = QSplitter(Qt.Horizontal)
+        self._list = ProjectListWidget()
+        self._detail = ProjectDetailWidget()
+
+        splitter.addWidget(self._list)
+        splitter.addWidget(self._detail)
+        splitter.setStretchFactor(0, 1)
+        splitter.setStretchFactor(1, 2)
+
+        layout.addWidget(splitter)
+
+        self._list.project_selected.connect(self._detail.load)
+        self._list.project_deleted.connect(self._detail.clear)
